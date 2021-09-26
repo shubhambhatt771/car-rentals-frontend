a5c2a21a3b9d5268c430ea4be9c65fe571dde275
x3shubham <shubhambhatt@raysteedsinfotech.com>
2021-09-26 22:36:23 +0530

    set port to 3002

--- src/config/default.js
+++ src/config/default.js
@@ -1,5 +1,5 @@
 const Config = {
-  "baseURL": "http://localhost:5500/api",
+  "baseURL": "http://localhost:3002/api",
   "s3_url": "https://uma-creatives.s3.ap-south-1.amazonaws.com/cars"
 }
 export default Config
